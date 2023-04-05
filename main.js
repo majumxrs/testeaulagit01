9734e7746d03a3f778e712479c7d44e9341092b8
majumxrs <129889573+majumxrs@users.noreply.github.com>
2023-04-05 12:19:50 -0300

    tentadiva de dividir

--- main.js
+++ main.js
@@ -14,6 +14,34 @@ function exibenome()
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é: " + usuario;
     document.getElementById( "nome" ).value = "";
 }
+
+document.getElementById("botao-somar").addEventListener("click" , somar);
+
+function somar()
+{
+    let numero1 = document.getElementById("numero1").value;
+    let numero2 = document.getElementById("numero2"). value;
+    document.getElementById( "resultado1" ).innerHTML = parseFloat(numero1) + parseFloat(numero2) ;
+}
+
+document.getElementById("botao-somar").addEventListener("click" , somar);
+
+function somar()
+{
+    let numero1 = document.getElementById("numero1").value;
+    let numero2 = document.getElementById("numero2"). value;
+    document.getElementById( "resultado1" ).innerHTML = parseFloat(numero1) + parseFloat(numero2) ;
+}
+
+document.getElementById("botao-dividir").addEventListener("click" , dividir);
+
+function dividir()
+{
+    let numero1 = document.getElementById("numero3").value;
+    let numero2 = document.getElementById("numero4"). value;
+    document.getElementById( "resultado2" ).innerHTML = parseFloat(numero3) / parseFloat(numero4) ;
+}
+
 document.getElementById("botao-somar").addEventListener("click" , somar);
 
 function somar()
